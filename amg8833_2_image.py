33078eef267a7abb3a4061c6d10d7bbd600ca377
HARAKEN0607 <113326732+HARAKEN0607@users.noreply.github.com>
2022-10-24 17:06:23 +0900

    add codes

---
+++ amg8833_2_image.py
@@ -0,0 +1,22 @@
+import time
+import busio
+import board
+
+import adafruit_amg88xx
+
+import matplotlib.pyplot as plt
+
+# I2Cバスの初期化
+i2c_bus = busio.I2C(board.SCL, board.SDA)
+
+# センサーの初期化
+sensor = adafruit_amg88xx.AMG88XX(i2c_bus, addr=0x68)
+
+# センサーの初期化待ち
+time.sleep(.1)
+
+# imshowでsensor.pixelsの２次元配列データを表示させる
+fig = plt.imshow(sensor.pixels, cmap="inferno")
+plt.colorbar()
+
+plt.show()
