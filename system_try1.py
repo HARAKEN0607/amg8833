4efea43da7834be42eedfc822a22d7483552cdec
HARAKEN0607 <113326732+HARAKEN0607@users.noreply.github.com>
2022-10-26 18:08:09 +0900

    add codes

--- system_try1.py
+++ system_try1.py
@@ -1,6 +1,5 @@
 from PIL import Image, ImageFont, ImageDraw
 import os
-import matplotlib.pyplot as plt
 
 folderpath = os.getcwd()
 
@@ -10,7 +9,8 @@ img = Image.open(image_path)
 
 d = ImageDraw.Draw(img)
 
-d.text((5, 5), '異常温度検知', fill='red')
+# d.text((200, 200), '異常温度検知', fill='blue')
+d.point([(50, 50), (150, 150)], fill='blue')
 
 img.save(folderpath + "/Processing_image.png")
 
