ef3c9dc88f5433f378087583e10a19202d92caff
HARAKEN0607 <113326732+HARAKEN0607@users.noreply.github.com>
2022-10-26 17:41:33 +0900

    add codes

--- system_try1.py
+++ system_try1.py
@@ -7,11 +7,11 @@ image_path = folderpath + '/img.png'
 
 image = Image.open(image_path)
 
-width = img.width
-height = img.height
+width = image.width
+height = image.height
 
-print("画像の高さは ",height)
-print("画像の幅は ",width)
+print("画像の高さは ", height)
+print("画像の幅は ", width)
 
 
 
