1e5ac7e03d39894de90733c55b6891a5fc782945
HARAKEN0607 <113326732+HARAKEN0607@users.noreply.github.com>
2022-10-26 16:56:36 +0900

    add codes

--- amg8833_1.py
+++ amg8833_1.py
@@ -14,3 +14,13 @@ time.sleep(0.2)
 
 # 8x8の温度配列
 print(sensor.pixels)
+
+list = sensor.pixels
+
+for n in range(0, 8, 1):
+    temp_arrange = list[n]
+
+    for d in range(0, 8, 1):
+        temp = temp_arrange[d]
+        if temp > 30:
+            print("異常温度を検知しました。  検知温度：" + str(temp) + '℃')
