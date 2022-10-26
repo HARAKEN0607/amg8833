dd3dd1c680fa0dc50b6834f52c453e779c80d717
HARAKEN0607 <113326732+HARAKEN0607@users.noreply.github.com>
2022-10-26 17:40:25 +0900

    add codes

--- system_try1.py
+++ system_try1.py
@@ -4,16 +4,14 @@ import os
 folderpath = os.getcwd()
 
 image_path = folderpath + '/img.png'
-# font_path = '/System/Library/Fonts/ヒラギノ角ゴシック W5.ttc';
-# font_size = 30  # 文字の大きさ
-# text = '異常検知'
-# color = (255, 255, 255)  # 文字の色
 
 image = Image.open(image_path)
-# font = ImageFont.truetype(font_path, font_size)  # フォントの指定
-# draw = ImageDraw.Draw(image)
-# draw.text((110, 20), text, font=font, fill=color)
-# image.save(image_path)
+
+width = img.width
+height = img.height
+
+print("画像の高さは ",height)
+print("画像の幅は ",width)
 
 
 
