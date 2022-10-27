2e476edde3012bdb97c09744b7001698ec518c3e
HARAKEN0607 <113326732+HARAKEN0607@users.noreply.github.com>
2022-10-27 14:50:26 +0900

    add codes

--- amg8833_image2.py
+++ amg8833_image2.py
@@ -16,7 +16,8 @@ sensor = adafruit_amg88xx.AMG88XX(i2c_bus, addr=0x68)
 time.sleep(.1)
 
 # imshowでsensor.pixelsの２次元配列データを表示させる
-fig = plt.imshow(sensor.pixels, cmap="inferno", interpolation="bicubic", labelbottom=False, labelleft=False)
+plt.axis("off")
+fig = plt.imshow(sensor.pixels, cmap="inferno", interpolation="bicubic")
 plt.colorbar()
 
 plt.savefig("img.png")
