983bb183c3a42c3478aae8ed8f1a5b63e50b9084
HARAKEN0607 <113326732+HARAKEN0607@users.noreply.github.com>
2022-10-27 13:37:56 +0900

    add codes

--- try1.py
+++ try1.py
@@ -1,14 +1,33 @@
 from PIL import Image, ImageFont, ImageDraw
 import os
+import time
+import busio
+import board
+
+import adafruit_amg88xx
 
 folderpath = os.getcwd()
 
-image_path = folderpath + '/Trimming_image.png'
 
-img = Image.open(image_path)
+def get_temp_data():
+    # I2Cの初期化
+    i2c = busio.I2C(board.SCL, board.SDA)
+    # サーモセンサーの初期化
+    sensor = adafruit_amg88xx.AMG88XX(i2c, addr=0x68)
 
-width = img.width
-height = img.height
+    # 初期化のために待つ
+    time.sleep(1)
 
-print("画像の高さは ", height)
-print("画像の幅は ", width)
+    # 8x8の温度配列
+    print(sensor.pixels)
+
+def trimming(left, upper, right, lower):  # trimming photo
+    image_path = folderpath + '/img.png'
+    img = Image.open(image_path)
+
+    im_crop = img.crop((left, upper, right, lower))
+    im_crop.save(folderpath + "/Trimming_image.png", quality=95)
+
+trimming(108, 58, 478, 428)
+
+get_temp_data()
