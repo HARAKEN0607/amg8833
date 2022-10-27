5184bd0ea41e1e81722784f6720db857b88d607b
HARAKEN0607 <113326732+HARAKEN0607@users.noreply.github.com>
2022-10-27 12:40:23 +0900

    add codes

--- trimming.py
+++ trimming.py
@@ -7,5 +7,6 @@ image_path = folderpath + '/img.png'
 
 img = Image.open(image_path)
 
-im_crop = img.crop((108, 47, 468, 422))
+im_crop = img.crop((107, 58, 478, 428))
 im_crop.save(folderpath + "/Trimming_image.png", quality=95)
+
