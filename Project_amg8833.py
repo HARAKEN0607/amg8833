2f4a5c144b298df0dc6e27e59e0e77f3d6296093
HARAKEN0607 <113326732+HARAKEN0607@users.noreply.github.com>
2022-10-28 21:04:24 +0900

    add codes

--- Project_amg8833.py
+++ Project_amg8833.py
@@ -47,7 +47,7 @@ def get_original_data():
     plt.imshow(sensor.pixels, cmap="inferno", interpolation="bicubic", vmin=temp_min, vmax=temp_max)
 
     # set color bar
-    plt.colorbar(extend='both')
+    plt.colorbar(extend='max')
 
     # original_photo saving
     plt.savefig("original_image.png")
