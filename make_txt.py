35a4250743f7cb8c1d4aa33b1a214368ab946d7c
HARAKEN0607 <113326732+HARAKEN0607@users.noreply.github.com>
2022-10-28 13:50:59 +0900

    add codes

--- make_txt.py
+++ make_txt.py
@@ -3,15 +3,42 @@ import os
 
 folderpath = os.getcwd()
 
-image_path = folderpath + '/img.png'
+image_path = folderpath + '/original_image.png'
 
 img = Image.open(image_path)
 
 d = ImageDraw.Draw(img)
 
-d.text((200, 200), 'Test Message\nExample', fill='blue', spacing=10, align='right')
+x = []
+y = []
 
-# d.line([(0, 50), (200, 50), (0, 150), (200, 150)], fill='blue', width=2)
+for n in range(0, 8, 1):
+    temp_arrange = list[n]
+
+    for m in range(0, 8, 1):
+        temp = temp_arrange[m]
+        if temp > 24:
+            x.append(m+1)
+            y.append(n+1)
+
+print(x)
+
+print(y)
+
+for n in range(0, len(y), 1):
+    x_point = 106+23*(2*x[n]-1)
+    y_point = 45+23*(2*y[n]-1)
+    print(x_point)
+    print(y_point)
+    d.text((x_point, y_point), '45', fill='blue', spacing=10, align='right')
+
+for n in range(0, len(y), 1):
+    x1_point = 106+46*(x[n]-1)
+    y1_point = 45+46*(y[n]-1)
+    x2_point = 106+46*x[n]
+    y2_point = 45+46*y[n]
+
+    d.ellipse((x1_point, y1_point, x2_point, y2_point), outline=(0, 0, 0))
 
 img.save(folderpath + "/Processing_image.png")
 
