fd82f2dd17e38d4ecd7e349991b8522f5d92cc88
HARAKEN0607 <113326732+HARAKEN0607@users.noreply.github.com>
2022-10-31 12:13:50 +0900

    add codes

--- Project_amg8833.py
+++ Project_amg8833.py
@@ -10,7 +10,7 @@ import adafruit_amg88xx
 
 dt_now = datetime.datetime.now()
 dt_now_name = dt_now.strftime('%Y%m%d%H%M%S')
-folderpath = os.getcwd() + '/' + dt_now_name + '/'
+folderpath = os.getcwd() + '/' + dt_now_name
 abn_temp = 23
 
 if not os.path.exists(folderpath):
