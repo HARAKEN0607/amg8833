76956b242f4b8bd460317ff078ced4e733d3dc6c
HARAKEN0607 <113326732+HARAKEN0607@users.noreply.github.com>
2022-12-08 14:34:43 +0900

    change d.text

--- txt_color.py
+++ txt_color.py
@@ -12,15 +12,11 @@ d = ImageDraw.Draw(img)
 # d.text((200, 200), '45', fill='white', outline='blue', spacing=10, align='right')
 # d.rectangle((200, 100, 300, 200), fill='white', outline='blue')
 
-text = "あいうえお"
-font = ImageFont.truetype(font="C:/WINDOWS/Fonts/Corbel", size=70)
-fill_color = (255, 0, 0)
-stroke_color = (0, 0, 255)
+draw = ImageDraw.Draw(img)
 
-d.text((50, 10), text, font=font, fill=fill_color, stroke_width=1, stroke_fill=stroke_color)
-d.text((50, 80), text, font=font, fill=fill_color, stroke_width=3, stroke_fill=stroke_color)
-d.text((50, 150), text, font=font, fill=fill_color, stroke_width=5, stroke_fill=stroke_color)
-d.text((50, 220), text, font=font, fill=fill_color, stroke_width=7, stroke_fill=stroke_color)
+# フォント TakaoExMincho を指定
+font = ImageFont.truetype('TakaoExMincho', 50)
+draw.text((100, 100), 'こんにちは', fill=(255, 0, 0), font=font)
 
 img.save(folderpath + "/Processing_image.png")
 
