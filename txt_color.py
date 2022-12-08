cb3331293e683150bf9aef224b7d96f79f84562f
HARAKEN0607 <113326732+HARAKEN0607@users.noreply.github.com>
2022-12-08 14:16:21 +0900

    change d.text

--- txt_color.py
+++ txt_color.py
@@ -9,8 +9,18 @@ img = Image.open(image_path)
 
 d = ImageDraw.Draw(img)
 
-d.text((200, 200), '45', fill='white', outline='blue', spacing=10, align='right')
-d.rectangle((200, 100, 300, 200), fill='white', outline='blue')
+# d.text((200, 200), '45', fill='white', outline='blue', spacing=10, align='right')
+# d.rectangle((200, 100, 300, 200), fill='white', outline='blue')
+
+text = "测试文字"
+font = ImageFont.truetype(font="C:/WINDOWS/Fonts/STKAITI.TTF", size=70)
+fill_color = (255, 0, 0)
+stroke_color = (0, 0, 255)
+
+d.text((50, 10), text, font=font, fill=fill_color, stroke_width=1, stroke_fill=stroke_color)
+d.text((50, 80), text, font=font, fill=fill_color, stroke_width=3, stroke_fill=stroke_color)
+d.text((50, 150), text, font=font, fill=fill_color, stroke_width=5, stroke_fill=stroke_color)
+d.text((50, 220), text, font=font, fill=fill_color, stroke_width=7, stroke_fill=stroke_color)
 
 img.save(folderpath + "/Processing_image.png")
 
