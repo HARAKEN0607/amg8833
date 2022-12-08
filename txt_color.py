75715390e40969d39d5c6a71cc258362f6476f9a
HARAKEN0607 <113326732+HARAKEN0607@users.noreply.github.com>
2022-12-08 15:47:15 +0900

    change d.text

--- txt_color.py
+++ txt_color.py
@@ -2,22 +2,21 @@ from PIL import Image, ImageFont, ImageDraw
 import os
 
 folderpath = os.getcwd()
-
 image_path = folderpath + '/original_image.png'
-
 img = Image.open(image_path)
-
 d = ImageDraw.Draw(img)
 
-# d.text((200, 200), '45', fill='white', outline='blue', spacing=10, align='right')
-# d.rectangle((200, 100, 300, 200), fill='white', outline='blue')
+# 図形縁取り
+d.rectangle((200, 100, 300, 200), fill='white', outline='blue')
 
-draw = ImageDraw.Draw(img)
-
-# フォント TakaoExMincho を指定
-font = ImageFont.truetype('TakaoExMincho', 50)
-draw.text((100, 100), 'こんにちは', fill=(255, 0, 0), font=font)
+# 文字列を描写
+font_path = 'C:\Windows\Fonts\meiryo.ttc'           # Windowsのフォントファイルへのパス
+font_size = 24                                      # フォントサイズ
+fnt = ImageFont.truetype(font_path, font_size)     # PILでフォントを定義
+d.text((100, 100), 'こんにちは', fill='blue', outline='white', font=fnt)
 
 img.save(folderpath + "/Processing_image.png")
 
 print('finished')
+
+
